fe0c0dbff8b5fe46c7004817556ff8c774b0a4a1
Smati Tungkitkancharoen <stungkit@gmail.com>
2017-02-28 21:26:17 -0800

    Started on Google's Tour of Heroes Angular2 Tutorial.com

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes!';
+  selectedHero: Hero;
+  heroes: Hero[];
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
